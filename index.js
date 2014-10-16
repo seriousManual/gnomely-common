3811ae21e5349a1292531fad842cf0d81ba5035c
zaphod1984 <zaphod84@gmx.de>
2014-10-16 22:10:39 +0200

    refactor commons

---
+++ index.js
@@ -0,0 +1,8 @@
+module.exports = {
+    loader: {
+        JobLoader: require('./lib/persistence/loader/JobLoader'),
+        OrganisationLoader: require('./lib/persistence/loader/OrganisationLoader'),
+        UserLoader: require('./lib/persistence/loader/UserLoader'),
+        WishLoader: require('./lib/persistence/loader/WishLoader')
+    }
+};
