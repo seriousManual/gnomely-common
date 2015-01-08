8385dfbd65b1da32be1916f62c96a048af0c626f
zaphod1984 <zaphod84@gmx.de>
2015-01-08 20:23:42 +0100

    add invite model

---
+++ tests/unit/Model.Invite.tests.js
@@ -0,0 +1,36 @@
+var mockgoose = require('mockgoose');
+var expect = require('chai').use(require('sinon-chai')).expect;
+var sinon = require('sinon');
+
+var mocks = require('../util/mocks');
+
+describe('InviteModel', function () {
+    var InviteModel, clock;
+
+    before(function () {
+        InviteModel = require('../../lib/persistence/model/Invite');
+        clock = sinon.useFakeTimers();
+    });
+
+    after(function () {
+        mockgoose.reset();
+        clock.restore();
+    });
+
+    describe('foo', function() {
+        var invite;
+
+        before(function(done) {
+            invite = new InviteModel({organisationIdent: 'fooOrgIdent', mail: 'fooMail', name: 'fooName'});
+            invite.save(done);
+        });
+
+        it('should', function() {
+            expect(invite.organisationIdent).to.equal('fooOrgIdent');
+            expect(invite.ack).to.be.false;
+            expect(invite.mail).to.equal('fooMail');
+            expect(invite.name).to.equal('fooName');
+            expect(invite.createDate.toISOString()).to.equal('1970-01-01T00:00:00.000Z');
+        });
+    });
+});
